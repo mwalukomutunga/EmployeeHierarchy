a2cf094a3b6e13cd0ab5b0e651b58e3b292daf84
Mwaluko <mwalukomutunga@gmail.com>
2021-07-02 03:13:41 +0300

    Added EmployeeHierarchy class

---
+++ EmployeeHierarchy/EmployeeHierarchy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeHierarchy
+{
+   public class EmployeeHierarchy
+    {
+        public EmployeeHierarchy(string employeeId, string managerId, int salary)
+        {
+            EmployeeId = employeeId;
+            ManagerId = managerId;
+            Salary = salary;
+            IsCeo = string.IsNullOrEmpty(managerId);
+        }
+        public string EmployeeId { get; private set; }
+        public string ManagerId { get; private set; }
+        public int Salary { get; private set; }
+        public bool IsCeo { get; private set; }
+
+    }
+}
